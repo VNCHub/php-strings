f90e76a89aac67cf22ccc207dd148653cc55b46b
Vinicius <viniciuscarrocine@gmail.com>
2023-11-07 02:31:49 -0300

    Finalizado funções de verificação

--- aula1.php
+++ aula1.php
@@ -2,4 +2,16 @@
 
 $nome = "Vinicius Carrocine";
 
-str_contains($nome,"Carrocine"); #verifica se contêm determinado valor
+$ehdaFamilia = str_contains($nome,"Carrocine"); # Verifica se contêm determinado valor
+
+if ($ehdaFamilia) {
+    echo "$nome é da familia Carrocine" . PHP_EOL;
+} else {
+    echo "$nome não é da familia Carrocine" . PHP_EOL;
+}
+
+# Verificações de começo e final de string
+$url = "https://google.com.br";
+
+$start = str_starts_with($url,"https");
+$end = str_ends_with($url,".br");
