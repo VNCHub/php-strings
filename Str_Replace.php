a809e935572eb9599f362696242d86b5af05afcf
Vinicius <viniciuscarrocine@gmail.com>
2023-11-18 10:13:20 -0300

    Alterado nomes de arquivos

--- Str_Replace.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$texto = "Uma frase com Palavrão e Palavrinha";
-
-echo str_replace(
-    ["Palavrão", "Palavrinha"],
-    "***",
-    $texto
-    ) . PHP_EOL;
--- aula3.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$nome = "Vinicius Carrocine%";
-
-$exploded = explode(" ", $nome); # Passa de String para array com base em um separador
-$imploded = implode("&", $exploded); # Passa de array para String
-$trimmed = trim($imploded, "%"); # Remove Caracteres especificos
-
-var_dump($trimmed);
